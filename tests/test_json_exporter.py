97591d3af6bd3f88f2c35cf154eb9c2161f98040
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-05-03 17:28:02 +0200

    use fixture for unit test setup/cleanup

--- tests/test_json_exporter.py
+++ tests/test_json_exporter.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 import datetime
 import json_exporter
@@ -5,17 +6,22 @@ import json_exporter
 test_json_file = 'test_json_file.json'
 
 
+@pytest.fixture()
 def setup():
     if os.path.exists(test_json_file):
         os.remove(test_json_file)
 
     exporter = json_exporter.json_exporter(json_file=test_json_file)
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    return exporter, current_date
+
+    yield exporter, current_date
+
+    if os.path.exists(test_json_file):
+        os.remove(test_json_file)
 
 
-def test_first_export():
-    exporter, current_date = setup()
+def test_first_export(setup):
+    exporter, current_date = setup
 
     exporter.update_json_file('math', 20)
     expected = f'{{\n    "{current_date}": {{\n        "math": 20\n    }}\n}}'
@@ -26,8 +32,8 @@ def test_first_export():
     assert data == expected, "first write failed"
 
 
-def test_update_existing_activity():
-    exporter, current_date = setup()
+def test_update_existing_activity(setup):
+    exporter, current_date = setup
 
     exporter.update_json_file('math', 20)
     exporter.update_json_file('math', 20)
@@ -39,8 +45,8 @@ def test_update_existing_activity():
     assert data == expected, "updating activity failed"
 
 
-def test_add_new_activity():
-    exporter, current_date = setup()
+def test_add_new_activity(setup):
+    exporter, current_date = setup
 
     exporter.update_json_file('math', 20)
     exporter.update_json_file('python', 15)
